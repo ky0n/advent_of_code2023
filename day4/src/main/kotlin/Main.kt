7bb780036ce7620af0b518e538c212823c7c4c27
Hendrik Schick <hendrik.schick@adcubum.com>
2023-12-13 19:01:56 +0100

    sixth start :)

--- day4/src/main/kotlin/Main.kt
+++ day4/src/main/kotlin/Main.kt
@@ -9,8 +9,16 @@ fun main(args: Array<String>) {
    println("Program arguments: ${args.joinToString()}")
 
    var lines = File("src/main/resources/input.txt").readLines()
+   val origNumScratchcards = lines.size
    var sum = 0.0
-   for (line in lines) {
+   var sum2 = 0
+   var copyMap = mutableMapOf<Int, Int>()
+   for (i in 0..<origNumScratchcards) {
+      copyMap[i] = 1
+   }
+
+   for (i in lines.indices) {
+      val line = lines[i]
       val allValue = line.substringAfter(":").trim()
       val ownValues = allValue.substringBefore("|").trim()
       val drawnValues = allValue.substringAfter("|").trim()
@@ -23,8 +31,19 @@ fun main(args: Array<String>) {
       if (hits > 0) {
          var inter = 2.0.pow((hits - 1).toDouble())
          sum += inter
+
+         for (j in 1..copyMap[i]!!) {
+            for (k in 1..hits) {
+               copyMap[i + k] = copyMap[i + k]!! + 1
+            }
+         }
       }
    }
 
+   sum2 = copyMap.values.sum()
    println(sum)
+
+   // too high: 9497017
+   println(sum2)
+
 }
