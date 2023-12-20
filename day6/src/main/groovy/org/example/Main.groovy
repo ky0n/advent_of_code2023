06e850891044df466643c684e02674a5eff0869f
Hendrik Schick <hendrik.schick@adcubum.com>
2023-12-21 00:42:59 +0100

    big integers .. +1

--- day6/src/main/groovy/org/example/Main.groovy
+++ day6/src/main/groovy/org/example/Main.groovy
@@ -5,6 +5,9 @@ static void main(String[] args) {
    def times = Arrays.asList(48, 93, 84, 66)
    def distances = Arrays.asList(261, 1192, 1019, 1063)
 
+   BigInteger time2 = 48938466
+   BigInteger distance2 = 261119210191063
+
    def numWins = []
    for (int i = 0; i < times.size(); i++) {
       def time = times.get(i)
@@ -26,4 +29,17 @@ static void main(String[] args) {
    }
 
    println(numWins.stream().reduce { a, b -> a * b})
+
+
+   def wins = 0
+   for (i in 0..<time2) {
+      def speedPerSecond = i
+      def restTime = time2 - speedPerSecond
+      def distanceBoat = restTime * speedPerSecond
+
+      if (distanceBoat >= distance2) {
+         wins = wins + 1
+      }
+   }
+   println(wins)
 }
