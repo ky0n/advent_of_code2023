e78b8b0eb3c986f90bebf2718475ff7cd2d6c162
Hendrik Schick <hendrik.schick@adcubum.com>
2023-12-28 00:22:38 +0100

    ...

--- day9/src/main/java/org/example/Main.java
+++ day9/src/main/java/org/example/Main.java
@@ -22,8 +22,7 @@ public class Main {
 
       BigInteger sum = new BigInteger("0");
       for (var line : lines) {
-         var foundSolution = false;
-         List<Integer> numbers = new ArrayList<>();
+         ArrayList<Integer> numbers = new ArrayList<>();
          Matcher matcher = pattern.matcher(line);
 
          while (matcher.find()) {
@@ -31,23 +30,39 @@ public class Main {
          }
 
          var allNewNums = new ArrayList<ArrayList<Integer>>();
-         while(!foundSolution) {
-            var nextNums = new ArrayList<Integer>();
-            var oldNum = numbers.get(0);
-            for (int i = 1; i < numbers.size(); i++) {
-               var num = numbers.get(i);
+         var nextNums = new ArrayList<Integer>();
+         var oldNum = numbers.get(0);
+
+         allNewNums.add(numbers);
+         var foundSolution = false;
+         while (!foundSolution) {
+            int num = 0;
+            nextNums.clear();
+            for (int i = 1; i < allNewNums.get(allNewNums.size() - 1).size(); i++) {
+               num = allNewNums.get(allNewNums.size() - 1).get(i);
                nextNums.add(num - oldNum);
 
-               oldNum = num;
             }
-            allNewNums.add(nextNums);
+            oldNum = num;
+            allNewNums.add(new ArrayList<>(nextNums));
+
             if (nextNums.stream().allMatch(n -> n == 0)) {
-               
+               foundSolution = true;
+               nextNums.add(0);
+               var lastNum = 0;
+               int lineSolution;
+               for (int j = allNewNums.size() - 2; j > 0; j--) {
+                  var l = allNewNums.get(j);
+                  var newNum = l.get(l.size() - 1) + lastNum;
+                  l.add(newNum);
+               }
+               lineSolution = allNewNums.get(0).get(allNewNums.get(0).size() - 1);
+               sum = sum.add(BigInteger.valueOf(lineSolution));
             }
-
          }
-
-//         sum.add(result);
       }
+      System.out.println("sum " + sum);
+
+      // 1079128959 too low
    }
 }
