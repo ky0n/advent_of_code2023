65b68db6438d041a2ef0e821b09638651c67e785
Hendrik Schick <hendrik.schick@adcubum.com>
2023-12-29 23:15:18 +0100

    first star day 9 :)

--- day9/src/main/java/org/example/Main.java
+++ day9/src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -14,7 +12,7 @@ import java.util.stream.Collectors;
 public class Main {
    public static void main(String[] args) throws IOException {
 
-      var pattern = Pattern.compile("\\d+");
+      var pattern = Pattern.compile("-?\\d+");
 
       var lines = Files.lines(Path.of(Main.class.getResource("/input.txt").getPath()))
             .map(String::trim)
@@ -36,25 +34,27 @@ public class Main {
          allNewNums.add(numbers);
          var foundSolution = false;
          while (!foundSolution) {
-            int num = 0;
             nextNums.clear();
             for (int i = 1; i < allNewNums.get(allNewNums.size() - 1).size(); i++) {
-               num = allNewNums.get(allNewNums.size() - 1).get(i);
+               var num = allNewNums.get(allNewNums.size() - 1).get(i);
                nextNums.add(num - oldNum);
-
+               oldNum = num;
             }
-            oldNum = num;
             allNewNums.add(new ArrayList<>(nextNums));
+            if (!nextNums.isEmpty()) {
+               oldNum = nextNums.get(0);
+            }
 
             if (nextNums.stream().allMatch(n -> n == 0)) {
                foundSolution = true;
                nextNums.add(0);
                var lastNum = 0;
                int lineSolution;
-               for (int j = allNewNums.size() - 2; j > 0; j--) {
+               for (int j = allNewNums.size() - 2; j >= 0; j--) {
                   var l = allNewNums.get(j);
                   var newNum = l.get(l.size() - 1) + lastNum;
                   l.add(newNum);
+                  lastNum = newNum;
                }
                lineSolution = allNewNums.get(0).get(allNewNums.get(0).size() - 1);
                sum = sum.add(BigInteger.valueOf(lineSolution));
@@ -64,5 +64,6 @@ public class Main {
       System.out.println("sum " + sum);
 
       // 1079128959 too low
+      // 2022497258 too high
    }
 }
